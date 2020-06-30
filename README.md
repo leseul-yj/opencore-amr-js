1dbf770d0daa1f9f788e881f3ac43322189e798c
leseul-yj <79645550@qq.com>
2020-06-30 18:29:20 +0800

    小结

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # opencore
 ## 音频加解密总结
-1. web不支持安卓的录音格式(amr格式的音频)，需要将amr格式的音频进行转码;
-2. 项目中从后端获取的文件是加密后的二进制，不能直接转码，应该先解密在进行转码;
+1. 初始直接将解密后的二进制文件转化成blob，建立url，更改audio的src，音频无法播放；
+2. web不支持安卓的录音格式(amr格式的音频)，需要将amr格式的音频进行转码;
+3. 项目中从后端获取的文件是加密后的二进制，不能直接转码，应该先解密在进行转码;
+4. audioContext 不支持amr
+```js
+		var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+				var source = audioCtx.createBufferSource();
+				audioCtx.decodeAudioData(ArrayBufferUtils.fromArray(decryptArray), function(decodedData) {
+					source.buffer = decodedData;
+					source.connect(audioCtx.destination);
+					source.start(0);
+				});
+
+   ```
+## 视频解密总结
+1. 最初使用mediaSource 但是会报错，说视频格式是非fragment，网络上的解决办法将视频转化格式，但是视频是从其他端传过的，转化格式不现实；
+2. 将blob的type设置为 'video/webm; codecs=h264'；
 
 
--- js/amr.util.js
+++ js/amr.util.js
@@ -1,5 +1,6 @@
 (function (global) {
     global.util = {
+        elementAudio: "",
         toString: function (blob, fn) {
 
             var reader = new FileReader();
@@ -8,38 +9,50 @@
             };
             reader.readAsBinaryString(blob);
         },
-        fetchBinaryString: function(url,callback){
+        fetchBinaryString: function (url, callback) {
             var xhr = new XMLHttpRequest();
             xhr.open('GET', url);
             xhr.responseType = 'blob';
-            xhr.onload = function() {
+            xhr.onload = function () {
                 callback(this.response);
             };
-            xhr.onerror = function() {
+            xhr.onerror = function () {
                 alert('Failed to fetch ' + url);
             };
             xhr.send();
         },
-         play: function (floats) {
+        play: function (floats) {
             var waveData = PCMData.encode({
                 sampleRate: 8000,
-                channelCount:   1,
+                channelCount: 1,
                 bytesPerSample: 2,
                 data: floats
             });
 
-            var element = new Audio();
-            element.src = "data:audio/wav;base64,"+btoa(waveData);
-            element.play();
-        }
+            this.elementAudio = new Audio();
+            this.elementAudio.src = "data:audio/wav;base64," + btoa(waveData);
+            this.elementAudio.play();
+        },
 
+        stop: function () {
+            if(this.elementAudio) {
+                this.elementAudio.pause()
+            }
+        },
+        destory: function () {
+            if(this.elementAudio) {
+                this.elementAudio.pause();
+                //this.elementAudio = null;
+            }
+        },
         /**
          * @author LearnBoost
          */
-        , merge: function (target, additional, deep, lastseen) {
-            var seen = lastseen || []
-                , depth = typeof deep == 'undefined' ? 2 : deep
-                , prop;
+        
+        merge: function (target, additional, deep, lastseen) {
+            var seen = lastseen || [],
+                depth = typeof deep == 'undefined' ? 2 : deep,
+                prop;
 
             for (prop in additional) {
                 if (additional.hasOwnProperty(prop) && seen.indexOf(prop) < 0) {
